a8cf0a2ca46b5c00e4065888fb7e26a7b23457f2
kunal768 <33108756+kunal768@users.noreply.github.com>
2019-10-02 14:41:10 +0530

    Update kadane.py

--- allalgorithms/subarray/kadane.py
+++ allalgorithms/subarray/kadane.py
@@ -1,4 +1,14 @@
 # Algorithm for Kadane's max sum contigious subarray
+# -*- coding: UTF-8 -*-
+#
+# Merge Sort Algorithm
+# The All ▲lgorithms library for python
+#
+# Contributed by: Kunal Keshav Singh Sahni 
+# Github: @kunal768
+#
+
+import sys
 
 def maxsum_subarray(arr):
 	curr = arr[0]
@@ -12,3 +22,24 @@ def maxsum_subarray(arr):
 	return maxx
 
 
+def returnArray(arr):
+    size = len(arr) 
+    max_so_far = -sys.maxsize - 1
+    max_ending_here = 0
+    start = 0
+    end = 0
+    s = 0
+    for i in range(0,size): 
+  
+        max_ending_here += arr[i] 
+  
+        if max_so_far < max_ending_here: 
+            max_so_far = max_ending_here 
+            start = s 
+            end = i 
+  
+        if max_ending_here < 0: 
+            max_ending_here = 0
+            s = i+1
+
+    return [max_so_far,arr[start:end+1]]
