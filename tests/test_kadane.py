73ab13e7ac7964057a79aea5a165d5f331bc7f5e
kunal768 <33108756+kunal768@users.noreply.github.com>
2019-10-03 22:23:26 +0530

    Update test_kadane.py

--- tests/test_kadane.py
+++ tests/test_kadane.py
@@ -1,19 +1,19 @@
 import unittest
 
-from allalgorithms.kadane import *
+from allalgorithms.subarray import kadane
 
 class TestSearches(unittest.TestCase):
 	def test_returnArray(self):
-		self.assertEqual( [11,[2,3,-1,7]], returnArray([2,3,-1,7]))
-		self.assertEqual([5,[2,3]], returnArray([2,3,-2,4]))
-		self.assertEqual([0, [0]], returnArray([-1,-1,-0,0]))
-		self.assertEqual([-1, [-1]], returnArray([-1]))
+		self.assertEqual( [11,[2,3,-1,7]], kadane.returnArray([2,3,-1,7]))
+		self.assertEqual([5,[2,3]], kadane.returnArray([2,3,-2,4]))
+		self.assertEqual([0, [0]], kadane.returnArray([-1,-1,-0,0]))
+		self.assertEqual([-1, [-1]], kadane.returnArray([-1]))
 
 	def test_maxsum_subarray(self):
-		self.assertEqual(11,maxsum_subarray([2,3,-1,7]))
-		self.assertEqual(5, maxsum_subarray([2,3,-2,4]))
-		self.assertEqual(0, maxsum_subarray([-1,-1,-0,0]))
-		self.assertEqual(-1, maxsum_subarray([-1]))
+		self.assertEqual(11,kadane.maxsum_subarray([2,3,-1,7]))
+		self.assertEqual(5, kadane.maxsum_subarray([2,3,-2,4]))
+		self.assertEqual(0, kadane.maxsum_subarray([-1,-1,-0,0]))
+		self.assertEqual(-1, kadane.maxsum_subarray([-1]))
 
 if __name__ == '__main__':
 	unittest.main()
