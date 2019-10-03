b7f3c53b8d5116af2e5f8f96fd460cf3187a9bcf
kunal768 <33108756+kunal768@users.noreply.github.com>
2019-10-03 22:00:42 +0530

    Update test_kadane.py

--- tests/test_kadane.py
+++ tests/test_kadane.py
@@ -1,17 +1,16 @@
 import unittest
-
 from allalgorithms.subarray import kadane
 
 class TestSearches(unittest.TestCase):
-
+    
 	def test_returnArray(self):
 		self.assertEqual( [11,[2,3,-1,7]], binary_search([2,3,-1,7]))
 		self.assertEqual([5,[2,3]], binary_search([2,3,-2,4]))
 		self.assertEqual([0, [0]], binary_search([-1,-1,-0,0]))
 		self.assertEqual([-1, [-1]], binary_search([-1]))
-
+        
 	def test_maxsum_subarray(self):
-        self.assertEqual(11,binary_search([2,3,-1,7]))
+		self.assertEqual(11,binary_search([2,3,-1,7]))
 		self.assertEqual(5, binary_search([2,3,-2,4]))
 		self.assertEqual(0, binary_search([-1,-1,-0,0]))
 		self.assertEqual(-1, binary_search([-1]))
