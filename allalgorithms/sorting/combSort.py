5d7cdfd746acb6bddc31bc2334290c57e38c65a4
JacksonMateus <48231382+JacksonMateus@users.noreply.github.com>
2019-10-18 15:56:44 -0300

    Create combSort.py

---
+++ allalgorithms/sorting/combSort.py
@@ -0,0 +1,11 @@
+def combsort(num):
+    gap = len(num)
+    swaps = True
+    while gap > 1 or swaps:
+        gap = max(1, int(gap / 1.25))  # minimum gap is 1
+        swaps = False
+        for i in range(len(num) - gap):
+            j = i+gap
+            if num[i] > num[j]:
+                num[i], num[j] = num[j], num[i]
+                swaps = True
