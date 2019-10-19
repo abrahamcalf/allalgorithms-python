c26c09d58daccd5b3924296a8d5c58e74fe6312c
JacksonMateus <48231382+JacksonMateus@users.noreply.github.com>
2019-10-18 23:13:39 -0300

    Create radixSort.py

---
+++ allalgorithms/sorting/radixSort.py
@@ -0,0 +1,22 @@
+def radixSort(A):
+    RADIX = 10
+    maxLength = False
+    tmp , placement = -1, 1
+
+    while not maxLength:
+        maxLength = True
+        buckets = [list() for _ in range(RADIX)]
+        for  i in A:
+            tmp = i / placement
+            buckets[tmp % RADIX].append(i)
+            if maxLength and tmp > 0:
+                maxLength = False
+
+        a = 0
+        for b in range(RADIX):
+            buck = buckets[b]
+            for i in buck:
+                A[a] = i
+                a += 1
+        placement *= RADIX
+    return A
